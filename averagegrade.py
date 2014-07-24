8d4530ec319cf4d718b212ad2cb7f3d952abc426
ethan fuller <ethan.fuller@outlook.com>
2014-07-24 19:55:19 -0400

    Create averagegrade.py

---
+++ averagegrade.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python2.2 -tt
+# Copyright 2014 Ethan Fuller. All Rights Reserved.
+
+def main():
+    print 'Start by entering your quarter grades.'
+    grade_one = input('Course one: ')
+    grade_two = input('Course one: ')
+    grade_three = input('Course one: ')
+    grade_four = input('Course one: ')
+    grade_five = input('Course one: ')
+    grade_six = input('Course one: ')
+
+if __name__ == '__main__':
+    main()
