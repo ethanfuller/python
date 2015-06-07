62684915d62f449f1783e0abd44c4f32ef52051b
ethan fuller <ethan.fuller@outlook.com>
2015-06-07 12:34:21 -0400

    Create test.py

---
+++ test.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python2.2 -tt
+# Copyright 2014 Ethan Fuller. All Rights Reserved.
+import os
+
+def main():
+    os.system('clear')
+    print 'GPA calculator by Ethan Fuller'
+    print 'Begin by entering your quarter grades. Only numbers.'
